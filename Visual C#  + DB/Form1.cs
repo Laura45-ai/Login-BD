39151bf5d336fa5c11c86d24a4b8194da820725e
laura45-ai <laura.colorado451@pascualbravo.edu.co>
2024-10-18 01:59:12 -0500

    se agregó

---
+++ Visual C#  + DB/Form1.cs
@@ -0,0 +1,159 @@
+﻿//Laura Cristina Colorado Sánchez
+
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Visual_C_____DB
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "server=localhost;database=iniciarseccion;user=root;password=1234567890;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                        try
+                        {
+                            conn.Open();
+                            string query = "UPDATE iniciarseccion SET columna2 = @correo, columna3 = @contraseña WHERE columna1 = @id";
+
+                            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@correo", textBox2.Text);
+                                cmd.Parameters.AddWithValue("@contraseña", textBox3.Text);
+                                cmd.Parameters.AddWithValue("@id", textBox1.Text); // ID
+
+                                int rowsAffected = cmd.ExecuteNonQuery();
+                                MessageBox.Show($"{rowsAffected} registros modificados.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: " + ex.Message);
+                        }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "DELETE FROM iniciarseccion WHERE Id = @id";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", textBox1.Text); // ID
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show($"{rowsAffected} registros eliminados.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO iniciarseccion (Id, Correo, Contraseña) VALUES (@Id, @Correo, @Contraseña)";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Id", textBox1.Text);
+                        cmd.Parameters.AddWithValue("@Correo", textBox2.Text);
+                        cmd.Parameters.AddWithValue("@Contraseña", textBox3.Text);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show($"{rowsAffected} registros insertados.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
